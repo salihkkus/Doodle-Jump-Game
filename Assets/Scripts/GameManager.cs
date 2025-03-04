02af1960b7c33e1fbe46080e031e2573f872e774
salihkarakus <sskarakussalih77@gmail.com>
2025-03-04 13:48:46 +0300

    controller class added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,3 +16,28 @@ public class GameManager : MonoBehaviour
         
     }
 }
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+
+    public void Jump(float force)
+    {
+        var velocity = rb.velocity;
+        velocity.y = force;
+        rb.velocity = velocity;
+    }
+}
+
+public class PlatformController : MonoBehaviour
+{
+    [SerializeField] private float force;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.transform.CompareTag("Player"))
+        {
+            var player = other.transform.GetComponent<PlayerController>();
+            player.Jump(force);
+        }
+    }
+}
