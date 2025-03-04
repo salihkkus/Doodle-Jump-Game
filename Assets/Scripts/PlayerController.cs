788b7fa9dd89a71d528c65599b19f9b60080087f
salihkarakus <sskarakussalih77@gmail.com>
2025-03-04 14:41:08 +0300

    move and aspect updated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] private float speed;
+    [SerializeField] private float limit;
+    
+    private bool isJumping => rb.velocity.y > 0.01f;
+    private float input;
 
     public void Jump(float force)
     {
+        if (isJumping) return;
+
         var velocity = rb.velocity;
         velocity.y = force;
         rb.velocity = velocity;
+    }
+
+    void Update()
+    {
+        input = Input.GetAxis("Horizontal");
+        if(input != 0)
+        {
+            spriteRenderer.flipX = input < 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var position = transform.position;
+
+        // **Önce input ile hareket et**
+        position.x += input * speed * Time.deltaTime;
+
+        // **Sonra Clamp ile sınırlandır**
+        position.x = Mathf.Clamp(position.x, -limit, limit);
+
+        transform.position = position;
     }
 }
